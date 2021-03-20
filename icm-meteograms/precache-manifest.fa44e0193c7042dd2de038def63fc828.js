a2f9d8a2b7b171377734d29d71771b76dc382c10
RobertW <riasol.rob@gmail.com>
2021-03-20 06:52:56 +0100

    icm-meteograms

---
+++ icm-meteograms/precache-manifest.fa44e0193c7042dd2de038def63fc828.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21384aa9b48e957051964aaa6b366e38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3f73779646a767b7632f",
+    "url": "./static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "a37f8296991e63142541",
+    "url": "./static/css/main.6c3b10fe.chunk.css"
+  },
+  {
+    "revision": "3f73779646a767b7632f",
+    "url": "./static/js/2.1f8ec936.chunk.js"
+  },
+  {
+    "revision": "74b5b99a348fedd13ab1cb018bec8748",
+    "url": "./static/js/2.1f8ec936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a37f8296991e63142541",
+    "url": "./static/js/main.779012d2.chunk.js"
+  },
+  {
+    "revision": "0c92ddcc6eb427025dff",
+    "url": "./static/js/runtime-main.eb9f11a7.js"
+  }
+]);
